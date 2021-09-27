595bba5d5a0b93bef231ba5feb5554dede5fd3f3
shantiswarooppreddy <shanti.s.reddy@gmail.com>
2021-09-28 00:27:55 +0530

    UpdateintheProgram

--- JavaPractice/src/CollectionsPractice/TreeSetComparator.java
+++ JavaPractice/src/CollectionsPractice/TreeSetComparator.java
@@ -6,7 +6,7 @@ public class TreeSetComparator {
 	
 	public static void main(String args[])
 	{
-		TreeSet<Student> ts = new TreeSet<Student>(new Student("Abhi", 1, "DPs"));
+		TreeSet<Student> ts = new TreeSet<Student>();
 		ts.add(new Student("Abhi", 2, "DPS"));
 		ts.add(new Student("Barg", 3, "IFS"));
 		ts.add(new Student("Zak", 2, "DPS"));
